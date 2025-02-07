2a7b75b13032f71c6292dfe4526858c07a6a8930
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-02-07 14:38:12 +0100

    Forward prototype

--- Pedro/forward.cpp
+++ Pedro/forward.cpp
@@ -3,7 +3,10 @@
 Position position;
 
 void forward(Joints joints) {
-    // ** forward kinematics function placeholder **
+    // forward kinematics computation function prototype
+    // position.x = a2 * cos(theta1) * cos(theta2) + a3 * cos(theta1) * cos(theta2 + theta3);
+    // position.y = a2 * sin(theta1) * cos(theta2) + a3 * sin(theta1) * cos(theta2 + theta3);
+    // position.z = d1 + a2 * sin(theta2) + a3 * sin(theta2 + theta3);
     move(joints);
 }
 
