02a31bde0ab3f9ecd1a564e9e0f4e18383adccb2
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-02-07 14:14:35 +0100

    Fixed case sensitive file names

--- Pedro/Pedro.h
+++
@@ -1,9 +0,0 @@
-#ifndef PEDRO_H
-#define PEDROH_H
-
-#include "Init.h"
-#include "Forward.h"
-#include "Inverse.h"
-#include "Motor.h"
-
-#endif
---
+++ Pedro/pedro.h
@@ -0,0 +1,9 @@
+#ifndef PEDRO_H
+#define PEDROH_H
+
+#include "init.h"
+#include "motor.h"
+#include "forward.h"
+#include "inverse.h"
+
+#endif
