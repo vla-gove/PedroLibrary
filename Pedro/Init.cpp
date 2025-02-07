f068f5b7448ba9b603dfe6722c4fb46d3ca99263
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-02-07 13:53:39 +0100

    Single function init

--- Pedro/Init.cpp
+++ Pedro/Init.cpp
@@ -1,5 +1,32 @@
-#include "Init.h"
+#include "init.h"
 
-Links link_lengths;
-Position end_effector_position;
-Joints joint_angles;
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+//starting position
+Joints joints = {0.0, 0.0, 0.0}; // <- default joint angles will be updated
+Position position = {0.0, 0.0, 0.0};
+
+void initServos() {
+    servo1.attach(SERVO1_PIN);
+    servo2.attach(SERVO2_PIN); 
+    servo3.attach(SERVO3_PIN);
+}
+
+
+void initRobotArm() {
+   
+    initServos();
+    
+    //write to default position
+    servo1.write(joints.angle1); 
+    servo2.write(joints.angle2); 
+    servo3.write(joints.angle3); 
+    
+}
+
+// single function init
+void robotInit() {
+    initRobotArm();
+}
