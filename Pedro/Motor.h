d43ec4d709ad55c86c91f20e23784e8ec956acc2
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-02-07 13:57:51 +0100

    Move function implementation

--- Pedro/Motor.h
+++ Pedro/Motor.h
@@ -1,16 +1,8 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-class Motor {
-  public:
-    Motor(int pin1, int pin2, int pin3);
-    void initialize(); 
-    void moveJoint(int jointNumber, float angle);
-  private:
-    Servo servo1;
-    Servo servo2;
-    Servo servo3;
-    int servoPins[3];
-};
+#include "init.h" // to access servo pinout and config
+
+void move(Joints joints);  
 
 #endif
