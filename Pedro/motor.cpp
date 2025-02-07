789029a3289392bcbffe69724b5f250d702b9555
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-02-07 16:32:55 +0100

    Safety notes

--- Pedro/motor.cpp
+++ Pedro/motor.cpp
@@ -1,5 +1,7 @@
 #include "motor.h"
 
+// define joint mobility, servo and angle limits, colision and singularity checks based on the configuration before moving joints
+
 void move(Joints joints) {
     servo1.write(joints.angle1); 
     servo2.write(joints.angle2); 
