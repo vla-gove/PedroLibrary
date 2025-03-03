4c1529bbb2bd87e0f50d0fb8ea4c1682a0937095
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-03-03 23:03:09 +0100

    Updated constants in prototype

--- Pedro/inverse.cpp
+++ Pedro/inverse.cpp
@@ -7,18 +7,18 @@ void Inverse(Position position) {
 // joints.angle1 = atan2(y, x) * 180.0 / PI;
 
 // r = sqrt(x*x + y*y);
-// s = z - d1;
+// s = z - D1;
 // L = sqrt(r*r + s*s);
 
 // + check for reachability
 
 // joint angle 3 calculations
-// cosAngle3 = (a2*a2 + a3*a3 - L*L) / (2 * a2 * a3);
+// cosAngle3 = (A2*A2 + A3*A3 - L*L) / (2 * A2 * A3);
 // joint.angle3 = acos(cosAngle3) * 180.0 / PI;
 
 // joint angle 2 calculations
 // alpha = atan2(s, r) * 180.0 / PI;
-// beta = acos((a2*a2 + L*L - a3*a3) / (2 * a2 * L)) * 180.0 / PI;
+// beta = acos((A2*A2 + L*L - A3*A3) / (2 * A2 * L)) * 180.0 / PI;
 // joints.angle2 = alpha + beta;
     
     move(joints);
