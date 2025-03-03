faa4c97c5229b103a7abf4eda3394cb93d663171
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-03-03 23:01:56 +0100

    Updated constants in prototype

--- Pedro/forward.cpp
+++ Pedro/forward.cpp
@@ -4,9 +4,9 @@ Position position;
 
 void forward(Joints joints) {
     // forward kinematics computation function prototype
-    // position.x = a2 * cos(theta1) * cos(theta2) + a3 * cos(theta1) * cos(theta2 + theta3);
-    // position.y = a2 * sin(theta1) * cos(theta2) + a3 * sin(theta1) * cos(theta2 + theta3);
-    // position.z = d1 + a2 * sin(theta2) + a3 * sin(theta2 + theta3);
+    // position.x = A2 * cos(theta1) * cos(theta2) + A3 * cos(theta1) * cos(theta2 + theta3);
+    // position.y = A2 * sin(theta1) * cos(theta2) + A3 * sin(theta1) * cos(theta2 + theta3);
+    // position.z = D1 + A2 * sin(theta2) + A3 * sin(theta2 + theta3);
     move(joints);
 }
 
