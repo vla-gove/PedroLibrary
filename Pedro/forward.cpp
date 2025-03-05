f9c6ddab2aab3c7675945a92271389b08bcd6a9c
vla-gove <107414426+vla-gove@users.noreply.github.com>
2025-03-05 23:18:05 +0100

    Forward Kinematics implementation

--- Pedro/forward.cpp
+++ Pedro/forward.cpp
@@ -1,12 +1,18 @@
 #include "forward.h"
+#include <math.h>
 
 Position position;
 
 void forward(Joints joints) {
-    // forward kinematics computation function prototype
-    // position.x = A2 * cos(theta1) * cos(theta2) + A3 * cos(theta1) * cos(theta2 + theta3);
-    // position.y = A2 * sin(theta1) * cos(theta2) + A3 * sin(theta1) * cos(theta2 + theta3);
-    // position.z = D1 + A2 * sin(theta2) + A3 * sin(theta2 + theta3);
+    float theta1 = joints.angle1 * PI / 180.0;
+    float theta2 = joints.angle2 * PI / 180.0;
+    float theta3 = joints.angle3 * PI / 180.0;
+    
+    // forward kinematics equations
+    position.x = A2 * cos(theta1) * cos(theta2) + A3 * cos(theta1) * cos(theta2 + theta3);
+    position.y = A2 * sin(theta1) * cos(theta2) + A3 * sin(theta1) * cos(theta2 + theta3);
+    position.z = D1 + A2 * sin(theta2) + A3 * sin(theta2 + theta3);
+    
     move(joints);
 }
 
